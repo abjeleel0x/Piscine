8be20b49158aea558a601af86b8ede9f589666b5
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:50:54 +0100

    FirstWord

--- CHECKPOINTS/LEVEL03/FirstWord.go
+++ CHECKPOINTS/LEVEL03/FirstWord.go
@@ -1,11 +1,9 @@
-package main
-
-import "fmt"
+package piscine
 
 func FirstWord(s string) string {
 	word := ""
 	for _, r := range s {
-		if r == ' ' {
+		if r == ' ' { // stop when first space found
 			break
 		}
 		word += string(r)
