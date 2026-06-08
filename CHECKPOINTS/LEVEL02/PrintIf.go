0a2510a60e46036c1a93b8f2e915598ff4564612
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:15:36 +0100

    PrintIf

--- CHECKPOINTS/LEVEL02/PrintIf.go
+++ CHECKPOINTS/LEVEL02/PrintIf.go
@@ -1,10 +1,25 @@
-package main
+Solution-1
 
-import "fmt"
+package piscine
 
 func PrintIf(str string) string {
-	if len(str) <= 0 || len(str) >= 3 {
+	if len(str) == 0 || len(str) >= 3 {
 		return "G\n"
 	}
 	return "Invalid Input\n"
+}
+
+Solution-2
+
+package piscine
+
+func PrintIf(str string) string {
+	if str == "" {
+		return "G\n"
+	}
+	if len(str) >= 3 {
+		return "G\n"
+	} else {
+		return "Invalid Input\n"
+	}
 }
