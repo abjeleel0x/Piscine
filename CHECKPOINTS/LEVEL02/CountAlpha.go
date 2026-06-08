d20b78c2c00123ad2b4fb8b54594c09d9ef15a21
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:12:24 +0100

    CountAlpha

--- CHECKPOINTS/LEVEL02/CountAlpha.go
+++ CHECKPOINTS/LEVEL02/CountAlpha.go
@@ -3,7 +3,7 @@ package piscine
 func CountAlpha(s string) int {
 	count := 0
 	for _, char := range s {
-		if ('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
 			count++
 		}
 	}
