f733060f5f6f3e066cadc30dbe10b7f4f98bd03b
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:06:33 +0100

    CheckNumber

--- CHECKPOINTS/LEVEL02/CheckNumber.go
+++ CHECKPOINTS/LEVEL02/CheckNumber.go
@@ -1,10 +1,10 @@
 package piscine
 
 func CheckNumber(arg string) bool {
-	for _, c := range arg {
-		if c >= '0' && c <= '9' {
+	for _, r := range arg {       // loop through each character (rune)
+		if r >= '0' && r <= '9' { // check if it's a digit
 			return true
 		}
 	}
-	return false
+	return false // no digits found
 }
