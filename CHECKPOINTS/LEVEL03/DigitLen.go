460c47970d1d3961570d397fb359d4109ad1fdf5
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:50:05 +0100

    DigitLen

--- CHECKPOINTS/LEVEL03/DigitLen.go
+++ CHECKPOINTS/LEVEL03/DigitLen.go
@@ -1,19 +1,17 @@
-package pisince
-
-import "fmt"
+package piscine
 
 func DigitLen(n, base int) int {
-	if base < 2 || base > 36 {
+	if base < 2 || base > 36 { // base must be valid
 		return -1
 	}
-	if n < 0 {
+	if n < 0 { // if negative, make it positive
 		n = -n
 	}
 	count := 0
 	for {
 		count++
 		n = n / base
-		if n == 0 {
+		if n == 0 { // stop when it reaches zero
 			break
 		}
 	}
