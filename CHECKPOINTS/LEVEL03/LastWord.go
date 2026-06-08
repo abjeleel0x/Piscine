63f390f8cbf28626b13810ffc7c591a7831c73b8
AB^JELEEL <thegr8tdhamie@gmail.com>
2026-06-08 13:57:15 +0100

    LastWord

--- CHECKPOINTS/LEVEL03/LastWord.go
+++ CHECKPOINTS/LEVEL03/LastWord.go
@@ -1,3 +1,5 @@
+Solution-1
+
 package piscine
 
 func LastWord(s string) string {
@@ -17,4 +19,32 @@ func LastWord(s string) string {
 		return "\n"
 	}
 	return s[start:end] + "\n"
+}
+
+Solution-2
+
+package piscine
+
+func LastWord(s string) string {
+	runes := []rune(s)
+	end := len(runes) - 1
+
+	// Step 1: skip trailing spaces
+	for end >= 0 && runes[end] == ' ' {
+		end--
+	}
+
+	if end < 0 {
+		return "\n"
+	}
+
+	// Step 2: find the start of the last word
+	start := end
+	for start >= 0 && runes[start] != ' ' {
+		start--
+	}
+
+	// Step 3: extract the word
+	word := string(runes[start+1 : end+1])
+	return word + "\n"
 }
